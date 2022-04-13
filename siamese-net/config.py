4b3536fa6386ff25ea75904870d0a0ba879c554b
Mohamed Yahyaoui <tpm.med1999@gmail.com>
2022-04-13 11:45:26 +0000

    modify link to data

--- siamese-net/config.py
+++ siamese-net/config.py
@@ -1,6 +1,6 @@
-training_dir = "/content/drive/My Drive/data/sign_data/train"
-training_csv = "/content/drive/My Drive/data/sign_data/train_data.csv"
-testing_csv = "/content/drive/My Drive/data/sign_data/test_data.csv"
-testing_dir = "/content/drive/My Drive/data/sign_data/test"
-batch_size = 32
+training_dir = "./content/drive/MyDrive/sign_data/train"
+training_csv = "./content/drive/MyDrive/sign_data/train_data.csv"
+testing_csv = "./content/drive/MyDrive/sign_data/test_data.csv"
+testing_dir = "./content/drive/MyDrive/sign_data/test"
+batch_size = 128
 epochs = 20
